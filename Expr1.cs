0ee796faacd6788269c9b3c2e638ccb0327249fc
DASFDASF <31626412+DASFDASF@users.noreply.github.com>
2017-09-15 23:37:34 +0300

    Add files via upload

---
+++ Expr1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Expr1. Как поменять местами значения двух переменных?
+            //Можно ли это сделать без ещё одной временной переменной. Стоит ли так делать?
+            //Латипов Камиль 
+            int a = 5;
+            int b = 6;
+             b = b - a;
+             a = b + a;
+             b = a - b;
+            Console.WriteLine(a.ToString(), b.ToString());
+
+
+        }
+    }
+}
---
+++ Expr4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int a = 30;
+            int b = a / x;
+            b += a / y;
+            b -= a / (x * y);
+            Console.WriteLine(b);
+        }
+    }
+}
