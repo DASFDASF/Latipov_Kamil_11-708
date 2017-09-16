834121db11a260ad94571adf5378c2d376ab6e2d
DASFDASF <31626412+DASFDASF@users.noreply.github.com>
2017-09-16 18:21:22 +0300

    Add files via upload

---
+++ 2017/FALL 2017/SEM/Expr3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int H = 3;
+            int b = 6 - H;
+            int c = 180 - Math.Abs(b) * 30;
+            Console.WriteLine(c);
+        }
+    }
+}
