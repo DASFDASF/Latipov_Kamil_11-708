b123c6774f918490bb808e4ccc78ba13bfa10b43
Latipov_Kamil <klatipov2012@litsey2.ru>
2017-09-21 17:41:46 +0300

    [add] Added solution of task with the rope and fence

---
+++ 2017/FALL 2017/SEM/Expr13.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr13
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            double lengthFence= 10;
+            double lengthRope = 6;
+             if (lengthRope  <= lengthFence/2)
+             {
+                double yardage = Math.PI * (lengthRope * lengthRope);
+                Console.WriteLine(yardage);
+
+             }
+             else if (lengthRope>= lengthFence*Math.Sqrt(2))
+             {
+                double yardage = lengthFence * lengthFence;
+                Console.WriteLine(yardage);
+            }
+             else
+             {
+                double angle = 2 * Math.Acos((lengthFence/2) / lengthRope);
+                double excessSegment = ((lengthRope * lengthRope) * (angle - Math.Sin(angle))) / 2;
+                double yardage = (Math.PI * (lengthRope * lengthRope)) - 4*excessSegment;
+                Console.WriteLine(yardage);
+            }
+            
+            
+           
+
+        }
+
+    }
+}
